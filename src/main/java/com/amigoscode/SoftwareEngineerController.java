94fed3c61f0ea8de7aa0e5300011c173b2f7cd00
joseaugtop <joseaugustop.rocha@unesc.net>
2026-02-19 17:37:58 -0300

    feat: add getById e post

--- src/main/java/com/amigoscode/SoftwareEngineerController.java
+++ src/main/java/com/amigoscode/SoftwareEngineerController.java
@@ -1,8 +1,6 @@
 package com.amigoscode;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,12 +8,30 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of (
-                new SoftwareEngineer(1,"James", "js, node, react, tailwindcss"),
-                new SoftwareEngineer(2, "Jamilla", "java, spring, springboot")
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
+
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(
+          @PathVariable Integer id
+    ) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+
+    }
+
+    @PostMapping
+    //normalmente é um dto (data transfer object)
+    public void addNewSoftwareEngineer( @RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 
 }
